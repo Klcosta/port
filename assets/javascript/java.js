98d047944d859f3a955466a3f8293ce26cc47d51
Costa <keith@Keiths-MacBook-Pro.local>
2019-06-26 21:17:42 -0400

    navbarfix

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -7,9 +7,9 @@ var config = {
     projectId: "portfolio-2894d",
     storageBucket: "portfolio-2894d.appspot.com",
     messagingSenderId: "429695162437"
-  };
-  firebase.initializeApp(config)
-  var database = firebase.database();
+};
+firebase.initializeApp(config)
+var database = firebase.database();
 
 //Necessary variables
 var name = ""
@@ -18,11 +18,11 @@ var message = ""
 
 $("#submit").on("click", function () {
     //Pull info and store in variables
-    name =  $("#name").val();
-    email =  $("#email").val();
+    name = $("#name").val();
+    email = $("#email").val();
     message = $("#message").val();
 
-    console.log (name, email, message)
+    console.log(name, email, message)
 
     //Push up to database
     database.ref().push({
@@ -36,7 +36,7 @@ $("#submit").on("click", function () {
     $("#submitrow").empty();
 
     //Fill Contact with Thanks Message
-    $("#contactform").html("<div class='col-12 border-0 rounded font green pl-4 py-3 rounded bg-white'> Dear " + name + ", <br><br>Thank you for reaching out. I greatly appreciate your interest in my work and services. I will get back in touch with you at " + email+ " as soon as possible.<br><br> Sincerely, Keith Costa</div>")
+    $("#contactform").html("<div class='col-12 border-0 rounded font green pl-4 py-3 rounded bg-white'> Dear " + name + ", <br><br>Thank you for reaching out. I greatly appreciate your interest in my work and services. I will get back in touch with you at " + email + " as soon as possible.<br><br> Sincerely, Keith Costa</div>")
 
     $("#contactform").addClass("p-3")
 
@@ -44,11 +44,11 @@ $("#submit").on("click", function () {
 
 $("#submitEs").on("click", function () {
     //Pull info and store in variables
-    name =  $("#name").val();
-    email =  $("#email").val();
+    name = $("#name").val();
+    email = $("#email").val();
     message = $("#message").val();
 
-    console.log (name, email, message)
+    console.log(name, email, message)
 
     //Push up to database
     database.ref().push({
@@ -62,7 +62,7 @@ $("#submitEs").on("click", function () {
     $("#submitrow").empty();
 
     //Fill Contact with Thanks Message
-    $("#contactform").html("<div class='col-12 border-0 rounded font green pl-4 py-3 rounded bg-white'> Estimado " + name + ", <br><br>Gracias por ponerte en contacto conmigo. Aprecio su interes en mi trabajo y servicios. Le mandare un mensage a " + email+ " lo antes possible.<br><br> Sinceramente, Keith Costa</div>")
+    $("#contactform").html("<div class='col-12 border-0 rounded font green pl-4 py-3 rounded bg-white'> Estimado " + name + ", <br><br>Gracias por ponerte en contacto conmigo. Aprecio su interes en mi trabajo y servicios. Le mandare un mensage a " + email + " lo antes possible.<br><br> Sinceramente, Keith Costa</div>")
 
     $("#contactform").addClass("p-3")
 
@@ -71,19 +71,23 @@ $("#submitEs").on("click", function () {
 //NAVBAR SCROLL
 
 
-$(window).scroll(function(){
-    $("#navbar").toggleClass("scroll" , $(this).scrollTop()> 10);
-    $(".dropdown").toggleClass("scroll" , $(this).scrollTop()>10);
+
+$(window).scroll(function () {
+    $("#navbar").toggleClass("scroll", $(this).scrollTop() > 10);
+    $(".dropdown").toggleClass("scroll", $(this).scrollTop() > 10);
     // $("#navbar").removeClass("scroll" , $(this).scrollTop() = 0.5);
-    $(".logo").toggleClass("scrollImg" , $(this).scrollTop()> 10); 
-    $("#testdiv").toggleClass("test1", $(this).scrollTop()> 10);
+    $(".logo").toggleClass("scrollImg", $(this).scrollTop() > 10);
+    $("#testdiv").toggleClass("test1", $(this).scrollTop() > 10);
 })
 
 //WHITE DROP DOWN MENU
-
 $("#white").on("click", (event) => {
-    $("#navbar").toggleClass("scroll") 
-    $(".dropdown").toggleClass("scroll")
+    if ($(window).scrollTop() === 0){
+
+        $("#navbar").toggleClass("scroll")
+        $(".dropdown").toggleClass("scroll")
+
+    }
 })
 
 //SPANISH TO ENGLISH TRANSLATE
